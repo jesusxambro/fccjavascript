c9c024b9e0f14c73911bfaf52fe0769c331df826
jesusxambro <jesus.ambrocio@outlook.com>
2022-01-29 08:33:34 -0600

    Added object access using bracket notation.

---
+++ accessing_objects.js
@@ -0,0 +1,13 @@
+//accessing objects using bracket notation
+
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+var entreeValue = testObj['an entree'];
+var drinkValue = testObj['the drink'];
+
+console.log(entreeValue);
+console.log(drinkValue);
